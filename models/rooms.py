a5d7e8857381cea685cf78b5567c8cc2d6429651
faithngetich <faith.ngetich@andela.com>
2017-04-13 16:59:35 +0300

    [Chore] create sub class Office

--- models/rooms.py
+++ models/rooms.py
@@ -2,3 +2,15 @@ class Room(object):
     def __init__(self, room_name):
         self.room_id = id(self)
         self.room_name = room_name
+
+class Office(Room):
+
+    room_type = "office" 
+
+    def __init__(self, room_name):
+        """Override the init method of Person superclass."""
+        super(Office, self).__init__(room_name)
+
+    def __str__(self):
+        """To make Staff class human readable."""
+        return "{}".format(self.room_name)
