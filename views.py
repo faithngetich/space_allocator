08c0046f38d68755f1f4a24a3aab08f73588fc2d
faithngetich <faith.ngetich@andela.com>
2017-04-13 17:48:46 +0300

    [Chore] Remove existing functionality

--- views.py
+++ views.py
@@ -8,94 +8,37 @@ from models.rooms import Room
 
 
 class Amity(object):
-    def __init__(self):
-        self.all_staff = []
-        self.all_fellows = [{"person_type" : "fellow", "person_name" : "Tetio"}]
-        self.living_spaces = [{"room_type" : "living_space", "room_name" : "Vazlhala"}]
-        self.offices = []
-        self.accomodation_list = []
-        self.allocated_rooms = {}
+    all_staff = []
+    all_fellows = [{"person_type" : "fellow", "person_name" : "Tetio"}]
+    living_spaces = [{"room_type" : "living_space", "room_name" : "Vazlhala"}]
+    offices = []
+    accomodation_list = []
+    allocated_rooms = {}
 
     def create_room(self, list_of_rooms, room_type):        
-        if room["room_type"] == "office":
-            for rooms in self.offices:
-                if room["room_name"] == rooms["room_name"]:
-                    print( "\n" + rooms.room_name.upper() + "already exists. \n")
-                    return
-                else:
-                    new_office = OfficeSpace(room["room_name"])
-                    self.offices.append(new_office)
-                    text = ("\n You have successfully added " + new_office.room_name.upper() + " to offices in amity")
-                    print(text)
-        else:
-            if self.living_spaces:
-                for rooms in self.living_spaces:
-                    if room["room_name"] == rooms["room_name"]:
-                        print( "\n" + rooms["room_name"].upper()+"already exists. \n")
-                        return
-                    else:
-                        new_livingSpace = room["room_name"]
-                        self.living_spaces.append(new_livingSpace)
-                        text = ("\n You have successfully added" + new_livingSpace.upper() + "to living space in amity")
-                        print(text)
-                        return
-# amity = Amity()
-# amity.create_room({"room_type" : "living_space", "room_name" : "Valhala"})
+        pass
 
     def add_person(self, person):
-        if person["person_type"] == "fellow":
-            for people in self.all_fellows:
-                if person["person_name"] == people["person_name"]:
-                    print("\n"+person["person_name"].upper()+"already exists.\n")
-                    return
-                else:
-                    new_person = person["person_name"]
-                    # new_person.accomodation = person["wants_accomodation"]
-                    self.all_fellows.append(new_person)
-                    print("you have added"+new_person.upper()+"as a staff to the list")
-                    return
-        else:
-            if self.staff:
-                for people in self.all_staff:
-                    if person["person_name"] == people["person_name"]:
-                        print("\n"+person["person_name"].upper()+"already exists.\n")
-                        return
-                    else:      
-                        new_person = person["staff_name"]
-                        new_person.accomodation = person["wants_accomodation"]
-                        self.all_fellows.append(new_person)
-                        print("you have added"+new_person.person.upper()+"as a fellow to the list")
-                        return
+        pass
 
-# amity = Amity()
-# amity.add_person({"person_type" : "fellow", "person_name" : "Ebrahim"})
+    def reallocate_person(self, person_id, new_room_name):
+        pass
+    
+    def print_allocations(self):
+        pass
+    
+    def print_unallocated(self):
+        pass
+    
+    def print_room(self, room_name):
+        pass
 
-    # def reallocate_person(self, name, newroom):
-    #     pass
+    def save_state(self):
+        pass
+    
+    def load_state(self):
+        pass
 
-    # def load_people():
-    #     pass
-
-    # def print_allocations(self):
-    #     session.query(Person).all()
-
-    # def print_unallocated(self, name):
-    #     pass
-
-    # def print_room(self, rname):
-    #     rs = ses.query(room).all()
-    #     for room in rs:
-    #         print(room.Name)
-
-    # def get_list_of_fellows(self):
-    #     return self.all_fellows
-
-    # def get_list_of_offices(self):
-    #     return self.offices
-
-    # def get_list_of_living_spaces(self):
-    #     return self.living_spaces
-
-    # def get_list_of_staff(self):
-    #     return self.all_staff
+    def load_people(self, filename):
+        pass
 
